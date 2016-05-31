21a357cc88078aa047ed6dc316d89ff00d458ab2
徐远翔 <litx@xuyuanxiangdemacbook-pro.local>
2016-05-31 17:52:51 +0800

    Initial Commit

---
+++ QYInotechSDK/QYInotechSDK.h
@@ -0,0 +1,13 @@
+//
+//  QYInotechSDK.h
+//  QYInotechSDK
+//
+//  Created by 徐远翔 on 16/5/31.
+//  Copyright © 2016年 Innotechx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QYInotechSDK : NSObject
+
+@end
---
+++ QYInotechSDK/QYInotechSDK.m
@@ -0,0 +1,13 @@
+//
+//  QYInotechSDK.m
+//  QYInotechSDK
+//
+//  Created by 徐远翔 on 16/5/31.
+//  Copyright © 2016年 Innotechx. All rights reserved.
+//
+
+#import "QYInotechSDK.h"
+
+@implementation QYInotechSDK
+
+@end
